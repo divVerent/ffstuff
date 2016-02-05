be02b3a3affff8ad5c04ac1ec2ca5d5a362acfbf
Rudolf Polzer <rpolzer@google.com>
2016-02-05 08:10:24 -0500

    Add support for the "subtitles" ffmpeg filter, and replace the existing -vf ass hack by that.
    Also, add support for Nexus 7, and improve the scaling formula.

--- ffnexus7.sh
+++ ffnexus7.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# iPhone 4 half resolution.
-w=480
-h=320
+# Nexus 7 resolution.
+w=1920
+h=1200
 sar=1/1
 dar=
 
 # min = optimize for zoomed out display
 # max = optimize for zoomed in display
-mode=max
+mode=min
 
 [ -n "$dar" ] || dar="($sar*$w/$h)"
 [ -n "$sar" ] || sar="($dar*$h/$w)"
@@ -33,13 +33,9 @@ ffencode.sh \
 		-b:a 96k \
 	-codec:v libx264 \
 		-threads:v 0 \
-		-maxrate:v 2500k \
-		-bufsize:v 1000k \
-		-rc_init_occupancy:v 900k \
-		-level:v 30 \
-		-profile:v baseline \
+		-profile:v high \
 		-tune:v animation \
-		-crf:v 25 \
+		-crf:v 23 \
 	-vf-post "scale=\
 			floor(min($w*$mode(1\\,dar/$dar)\\,in_w*$mode(1\\,sar/$sar))/2+0.5)*2:\
 			floor(min($h*$mode($dar/dar\\,1)\\,in_h*$mode($sar/sar\\,1))/2+0.5)*2" \
