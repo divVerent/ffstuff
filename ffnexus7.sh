db211f34e89e7d16e9dac3c3ccad226cad368b0e
Rudolf Polzer <divverent@xonotic.org>
2016-02-05 18:06:02 +0100

    Fix subtitle stream selection. It works!

--- ffnexus7.sh
+++ ffnexus7.sh
@@ -37,7 +37,7 @@ ffencode.sh \
 		-tune:v animation \
 		-crf:v 23 \
 	-vf-post "scale=\
-			floor(min($w*$mode(1\\,dar/$dar)\\,in_w*$mode(1\\,sar/$sar))/2+0.5)*2:\
-			floor(min($h*$mode($dar/dar\\,1)\\,in_h*$mode($sar/sar\\,1))/2+0.5)*2" \
+			floor(min($w*$mode(1\\,dar/$dar)\\,in_w*max(1\\,sar/$sar))/2+0.5)*2:\
+			floor(min($h*$mode($dar/dar\\,1)\\,in_h*max($sar/sar\\,1))/2+0.5)*2" \
 	-vf-post "setsar=1/1" \
 	"$@"
