07d0b902e107e969f05d766a8957d50d92b63d45
Rudolf Polzer <divVerent@gmail.com>
2022-01-24 11:48:01 -0800

    Add some generic profiles.

---
+++ ff720p.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Typical 720p.
+w=1280
+h=720
+sar=1/1
+dar=
+
+# min = optimize for zoomed out display
+# max = optimize for zoomed in display
+mode=min
+
+[ -n "$dar" ] || dar="($sar*$w/$h)"
+[ -n "$sar" ] || sar="($dar*$h/$w)"
+
+# size must be multiples of this
+div=2
+
+conf=$1
+shift
+in=$1
+shift
+out=$1
+shift
+
+ffencode.sh \
+	"$conf" "$in" "$out" \
+	-y \
+	-nostdin \
+	-f mp4 \
+		-movflags +faststart \
+	-vsync vfr -r 120000 \
+	-codec:a libfdk_aac \
+		-b:a 96k \
+	-codec:v libx264 \
+		-threads:v 0 \
+		-profile:v high \
+		-tune:v animation \
+		-crf:v 23 \
+	-vf-post "scale=\
+			floor(min($w*$mode(1\\,dar/$dar)\\,in_w*max(1\\,sar/$sar))/2+0.5)*2:\
+			floor(min($h*$mode($dar/dar\\,1)\\,in_h*max($sar/sar\\,1))/2+0.5)*2" \
+	-vf-post "setsar=$sar" \
+	"$@"
